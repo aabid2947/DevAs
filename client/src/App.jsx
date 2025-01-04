dac81092887b742cec5a3549e59f58fa8f383632
aabid2947 <aabidhussainpas@gmail.com>
2025-01-04 11:03:33 +0530

    project Initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-black text-xl font-bold'>Hello </h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
